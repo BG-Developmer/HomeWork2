e3ad40765b51509d7189c85d673b2f9de0f49a85
BG-Developmer <123984342+BG-Developmer@users.noreply.github.com>
2023-02-03 13:35:54 +0300

    Add files via upload

---
+++ createx/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+var mixer = mixitup('.directions__list');
+
+$('.directions-filter-btn').on('click', function () {
+    $('.directions-filter-btn').removeClass('directions-filter-btn--active')
+    $(this).addClass('directions-filter-btn--active')
+})
+
+})
+
